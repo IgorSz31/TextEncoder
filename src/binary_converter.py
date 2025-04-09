1aac468f21ed1010fb45a882f296c44049c591b8
IgorSz31 <igor.szymacha@lo31.krakow.pl>
2025-04-09 18:31:02 +0200

    Finished the symbol dictionary Created a function that searchers for provided symbol in the BINARY_LETTER_TABLE and returns its value. Created a function that returns provided string in the utf-8 code.

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -7,8 +7,28 @@ class BinaryConverter:
         self.binary_letter_table = binary_letter_table
 
     def single_letter_convert(self, letter):
-        pass
-        
+        """
+        Searchers for provided symbol in the BINARY_LETTER_TABLE and returns its value
+        """
+        output = ''
+        if letter in self.binary_letter_table:
+            output += self.binary_letter_table[letter]
+        return output
+
+    def user_words_iterator(self,word):
+        """
+        Returns provided string in the utf-8 code
+        """
+        output=''
+        letter_table = []
+        for letter in word:
+            letter_table.append(letter)
+        for letters in letter_table:
+            if letters in self.binary_letter_table:
+                output +=self.binary_letter_table[letters] + ' '
+        return output
+
 
 converter = BinaryConverter(BINARY_LETTER_TABLE)
-print(converter.single_letter_convert('a'))
+print(converter.single_letter_convert('Ą'))
+print(converter.user_words_iterator('abc'))
