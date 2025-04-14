6b37d0ff4fe762fcf3f12dfc4caeb5c503ef31f3
IgorSz31 <igor.szymacha@lo31.krakow.pl>
2025-04-14 18:23:06 +0200

    made the code clearer by using already existing functions in the proper way as well as creating new ones

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -6,29 +6,32 @@ class BinaryConverter:
     def __init__(self, binary_letter_table: dict):
         self.binary_letter_table = binary_letter_table
 
-    def single_letter_convert(self, letter):
+    def single_letter_convert(self, letter: str) -> str:
         """
         Searchers for provided symbol in the BINARY_LETTER_TABLE and returns its value
         """
-        output = ''
-        if letter in self.binary_letter_table:
-            output += self.binary_letter_table[letter]
-        return output
+        converted_letter = self.binary_letter_table[letter]
+        return converted_letter
 
-    def user_words_iterator(self,word):
+    def letter_list_create(self,word: str) -> list:
+
+        letter_list = [letter for letter in word]
+        return letter_list
+
+    def user_words_iterator(self,word: str) -> str:
         """
         Returns provided string in the utf-8 code
         """
         output=''
-        letter_table = []
-        for letter in word:
-            letter_table.append(letter)
-        for letters in letter_table:
-            if letters in self.binary_letter_table:
-                output +=self.binary_letter_table[letters] + ' '
+        letter_table = self.letter_list_create(word)
+        for letter in letter_table:
+            if letter in self.binary_letter_table:
+                output += self.single_letter_convert(letter) + ' '
+            else:
+                print(f"Symbol not found: {letter}")
         return output
 
 
 converter = BinaryConverter(BINARY_LETTER_TABLE)
-print(converter.single_letter_convert('Ą'))
-print(converter.user_words_iterator('abc'))
+# print(converter.single_letter_convert('['))
+print(converter.user_words_iterator('a[]bc'))
