7a4d4fc4e07ed5c08f31ab41b0946c0433f91794
IgorSz31 <igor.szymacha@lo31.krakow.pl>
2025-04-23 16:06:18 +0200

    code refactor

--- src/binary_converter.py
+++ src/binary_converter.py
@@ -10,28 +10,49 @@ class BinaryConverter:
         """
         Searchers for provided symbol in the BINARY_LETTER_TABLE and returns its value
         """
-        converted_letter = self.binary_letter_table[letter]
+        converted_letter = self.binary_letter_table[letter] + ' '
         return converted_letter
 
     def letter_list_create(self,word: str) -> list:
-
+        """
+        Creates a list of all letters in the given word
+        """
         letter_list = [letter for letter in word]
         return letter_list
+
+    def symbol_presence_validate(self,letter: str) -> bool:
+        """
+        Returns a bool basing on whether the given letter is present in the BINARY_LETTER_TABLE
+        """
+        if letter in self.binary_letter_table:
+            return True
+        else:
+            return False
+    def non_existent_symbols_listed(self,letter: str) -> list:
+        letter_table = self.letter_list_create(letter)
+        
+        pass
 
     def user_words_iterator(self,word: str) -> str:
         """
         Returns provided string in the utf-8 code
         """
-        output=''
+        output = ''
         letter_table = self.letter_list_create(word)
         for letter in letter_table:
-            if letter in self.binary_letter_table:
-                output += self.single_letter_convert(letter) + ' '
+            if self.symbol_presence_validate(letter):
+                output += self.single_letter_convert(letter)
+                ' '.join(output)
             else:
-                print(f"Symbol not found: {letter}")
+                print(f'Symbol not found: {letter}')
+            # if letter in self.binary_letter_table:
+            #     output += self.single_letter_convert(letter) + ' '
+            # else:
+            #     print(f"Symbol not found: {letter}")
         return output
 
 
 converter = BinaryConverter(BINARY_LETTER_TABLE)
 # print(converter.single_letter_convert('['))
-print(converter.user_words_iterator('a[]bc'))
+print(converter.user_words_iterator('abc'))
+
