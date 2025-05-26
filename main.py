d5ce4fa042f442c2828a2a743faadd35d9884874
IgorSz31 <igor.szymacha@lo31.krakow.pl>
2025-05-26 16:21:48 +0200

    Created command line interface using argparse

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-
+import argparse
 from src.binary_converter import BinaryConverter
 
 
 def main():
     converter = BinaryConverter()
-    x = converter.user_words_iterator('')
+    x = converter.user_words_iterator(input("Enter a word: "))
     print(x['status'])
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start_program', help='Starts program in command line')
+    args = parser.parse_args()
+    if args.start_program == 'console'.lower():
+        main()
+    elif args.start_program == 'ui'.lower():
+        print('--- PLACE HOLDER ---')
