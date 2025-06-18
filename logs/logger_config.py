0dd0679be88c6b851cda137f312323c5483b9145
IgorSz31 <igor.szymacha@lo31.krakow.pl>
2025-06-18 20:37:41 +0200

    Added a scrollbar to the ui mode so its more readable. Added missing docstrings

--- logs/logger_config.py
+++ logs/logger_config.py
@@ -3,5 +3,9 @@ import logging
 
 
 def setup_logger():
+    """
+    Sets up the logger file, its direction and format
+    """
+
     logging.basicConfig(level=logging.DEBUG, filename='logs/app.log',
                         filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
--- ui/ui_manager.py
+++ ui/ui_manager.py
@@ -2,7 +2,7 @@ import logging
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (QApplication, QWidget,
                                QMainWindow, QPushButton,
-                               QLineEdit, QVBoxLayout, QLabel)
+                               QLineEdit, QVBoxLayout, QLabel, QScrollArea,)
 import sys
 from src.binary_converter import BinaryConverter
 
@@ -10,45 +10,47 @@ logger = logging.getLogger(__name__)
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        """
+        Sets up the main window and its widgets
+        """
         super().__init__()
 
         self.converter = BinaryConverter()
 
-        self.setWindowTitle('Text Encoder')  # Window Title
+        self.setWindowTitle('Text Encoder')
         self.label = QLabel(self)
         self.label.setText('Enter string: ')
         self.label.setWordWrap(True)
         self.label.setTextInteractionFlags(Qt.TextSelectableByKeyboard|Qt.TextSelectableByMouse)
 
+        self.scroll_area = QScrollArea()
+        self.scroll_area.setWidgetResizable(True)
+        self.scroll_area.setWidget(self.label)
+
         self.input = QLineEdit(self)
-        self.button = QPushButton('Encode')  # Button
+        self.button = QPushButton('Encode')
         self.button.setCheckable(True)
         self.button.clicked.connect(self.btn_clicked)
 
-
-        layout = QVBoxLayout() # Vertical Layout
-        layout.addWidget(self.input)  #
-        layout.addWidget(self.label)  #  Adding all widgets to layout
-        layout.addWidget(self.button) #
-
-
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.scroll_area)
+        layout.addWidget(self.button)
 
         container = QWidget(self)
         container.setLayout(layout)
-
 
         self.setCentralWidget(container)
 
         logger.info('App layout loaded successfully')
 
     def btn_clicked(self):
+        """
+        Starts convertion mechanism. Reffered to by event listener
+        """
         x = self.converter.user_words_iterator(self.input.text())
         self.label.setText(f'{x['status']} {x['output']}')
         self.label.setWordWrap(True)
-
-
-
-
 
 # if __name__ == "__main__":
 #     app = QApplication(sys.argv)
